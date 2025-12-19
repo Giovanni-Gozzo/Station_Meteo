b67f056f5af50129a7f7e7616755558092e2d307
Giovanni-Gozzo <gio.gozzo84@gmail.com>
2025-12-19 15:40:39 +0100

    Fix pylint errors in main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
-from app.controllers.meteo_controller import MeteoController
-import pandas as pd
-from datetime import datetime
+"""
+Point d'entrée de l'application Flask.
+"""
 import os
+from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+
+# Import local modules
+from app.controllers.meteo_controller import MeteoController
 from app.config import CONFIG
 
 app = Flask(__name__)
@@ -14,9 +18,9 @@ def get_sort_key(meteo_obj, sort_by):
     """Retourne la valeur de l'attribut à utiliser comme clé de tri."""
     if sort_by == 'date':
         return meteo_obj.get_date()
-    elif sort_by == 'station_id':
+    if sort_by == 'station_id':
         return meteo_obj.get_station().get_station_id()
-    elif sort_by == 'ville':
+    if sort_by == 'ville':
         return meteo_obj.get_station().get_ville()
     return meteo_obj.get_date()
 
@@ -24,13 +28,14 @@ def get_sort_key(meteo_obj, sort_by):
 # --- Nouvelle Route d'Accueil : Afficher la sélection de station ---
 @app.route("/")
 def select_station():
+    """Route pour sélectionner la station."""
     try:
         file_path = os.path.join(CONFIG["DATA_DIR"], CONFIG["METEO_IDS_FILE"])
         df = pd.read_csv(file_path)
         all_ids = df["dataset_id"].tolist()
         unique_station_ids = sorted(list(set(all_ids)))
 
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-exception-caught
         print(f"Erreur lors de la lecture du CSV pour les IDs: {e}")
         unique_station_ids = []
 
@@ -40,6 +45,7 @@ def select_station():
 # --- Nouvelle Route pour Afficher les Données Filtrées ---
 @app.route("/data")
 def show_meteo_data():
+    """Route pour afficher les données météo d'une station."""
     selected_station_ids = request.args.getlist('station_id')
 
     if not selected_station_ids:
@@ -51,7 +57,7 @@ def show_meteo_data():
 
         unique_station_ids = sorted(list(set(selected_station_ids)))
 
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-exception-caught
         print(f"Erreur lors du chargement des données pour {selected_station_ids}: {e}")
         filtered_data = []
         unique_station_ids = []
@@ -59,7 +65,7 @@ def show_meteo_data():
     sort_by = request.args.get('sort', CONFIG["DEFAULT_SORT"])
     order = request.args.get('order', CONFIG["DEFAULT_ORDER"])
 
-    reverse_order = (order == 'desc')
+    reverse_order = order == 'desc'
 
     sorted_data = sorted(
         filtered_data,
